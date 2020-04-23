2ea3d8e0b70d36b1fc1d6e22e0e85007020e1145
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-04-23 12:17:49 +0200

    - separate loader test to a single and multi-process test

--- tests/test.py
+++ tests/test.py
@@ -48,7 +48,22 @@ class Tests(unittest.TestCase):
         loss_fn = ContagionLoss(allow_multiple_exposures=False)
         loss = loss_fn(batch, output)
 
-    def test_loader(self):
+    def test_loader_single_proc(self):
+        from loader import get_dataloader
+
+        path = "../data/0-risks"
+        batch_size = 5
+        dataloader = get_dataloader(
+            batch_size=batch_size, shuffle=False, num_workers=0, path=path
+        )
+        batch = next(iter(dataloader))
+        self.assertEqual(len(batch), 10)
+        # Testing that all the keys in the batch have the batch_size
+        keys_in_batch = list(batch.keys())
+        for key in keys_in_batch:
+            self.assertEqual(len(batch[key]), batch_size)
+
+    def test_loader_multi_proc(self):
         from loader import get_dataloader
 
         path = "../data/0-risks"
